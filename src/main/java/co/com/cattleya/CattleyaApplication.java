cf2de12062f87a533f2ab7e5448eb38324233bba
Jimmy Castro <ja.castros@javeriana.edu.co>
2023-04-20 15:44:00 -0500

    Auth context + foder structure

--- src/main/java/co/com/cattleya/CattleyaApplication.java
+++ src/main/java/co/com/cattleya/CattleyaApplication.java
@@ -2,10 +2,8 @@ package co.com.cattleya;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class CattleyaApplication {
     public static void main(String[] args) {
         SpringApplication.run(CattleyaApplication.class, args);
---
+++ src/main/java/co/com/cattleya/auth/infrastructure/security/WebSecurityConfig.java
@@ -0,0 +1,9 @@
+package co.com.cattleya.auth.infrastructure.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebSecurityConfig {
+
+}
